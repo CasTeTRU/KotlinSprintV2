1b35b60dc1f5d4d7446644cdf8d2f9af9a049456
CasTeT <kongalkin@gmail.com>
2024-08-25 00:33:54 +0300

    KS-1-3 add year hour and minutes

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,5 +1,20 @@
 package org.example.lesson_1
 
 fun main() {
-    println("Тестовое решение к уроку 1 третьей задачи")
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+
+    // Используем padStart чтобы выводить 09, а не 9.
+    // Иначе не даёт присваивать нули в начале целочисленных переменных
+
+    println("$year " +
+            "\n${hour.toString().padStart(2, '0')} " +
+            "\n${minute.toString().padStart(2, '0')} ")
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute")
 }
