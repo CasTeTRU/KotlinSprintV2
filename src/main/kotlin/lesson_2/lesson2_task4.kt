a002501dbd9ef38944861c8faec8b81ad415cd4c
CasTeT <kongalkin@gmail.com>
2024-08-25 11:44:26 +0300

    KS-2-4 Buff

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main() {
+    val crystalOre: Short = 7
+    val ironOre:Short = 11
+    val buffAmount = 0.2
+
+    println("Кристаллической руды под баффом добыто дополнительно: " +  (crystalOre * buffAmount).toInt())
+    println("Железной руды под баффом добыто дополнительно: " +  (ironOre * buffAmount).toInt())
+
+}
