f74064051020e24d88b326a669984c7711c7e7dd
CasTeT <kongalkin@gmail.com>
2024-08-25 22:15:23 +0300

    KS-4-1 Tables reservations

---
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_4
+
+fun main() {
+    val totalAmountOfTables = 13
+    val reservedTablesToday = 13
+    val reservedTablesTomorrow = 9
+
+    println("[Доступность столиков на сегодня: ${reservedTablesToday < totalAmountOfTables}]" +
+    "\n[Доступность столиков на завтра: ${reservedTablesTomorrow < totalAmountOfTables}]")
+
+}
+
