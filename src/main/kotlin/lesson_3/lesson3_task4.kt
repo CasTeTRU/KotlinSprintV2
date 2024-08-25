36e5cc05eeea73bcec9f439ce41338419c1d49c5
CasTeT <kongalkin@gmail.com>
2024-08-25 21:27:12 +0300

    KS-3-4 Chess

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    var startingSquare = "E2"
+    var endingSquare = "E4"
+    var turnNumber = 1
+
+    println("[$startingSquare-$endingSquare; $turnNumber]")
+
+    startingSquare = "D2"
+    endingSquare = "D3" //что имелось в виду под определить значение? посчитать самому?
+    turnNumber = 2
+
+    println("[$startingSquare-$endingSquare; $turnNumber]")
+}
