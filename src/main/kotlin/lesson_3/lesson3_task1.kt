a53ec3a292a8ff63eb3f2ddd3306f4758f239009
CasTeT <kongalkin@gmail.com>
2024-08-25 12:11:19 +0300

    KS-3-1 Greeting

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName = "Василий"
+    var greetingText = "Добрый день, "
+    println(greetingText + userName + "!")
+    greetingText = "Добрый вечер, "
+    println(greetingText + userName + "!")
+}
