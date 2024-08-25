dc8fe8cd5d226e521edb37e57d2e92ee6e992ad0
CasTeT <kongalkin@gmail.com>
2024-08-25 22:43:10 +0300

    KS-1-1-Gratitude for visiting Changed var to val in numberOfOrders

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_1
 
 fun main() {
-    var numberOfOrders: Int = 75
+    val numberOfOrders: Int = 75
     val gratitudeTitle: String = "Благодарим за посещение магазина"
 }
