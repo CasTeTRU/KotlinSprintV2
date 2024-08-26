7c0d8a8bb1e6c87ffbe7f7f6067134fbe6f1e639
CasTeT <kongalkin@gmail.com>
2024-08-26 18:37:26 +0300

    KS-1-3-Output Year hour and minutes Changed hour and minute to string type

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,20 +1,12 @@
 package org.example.lesson_1
 
 fun main() {
-    val year = 1961
-    var hour = 9
-    var minute = 7
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+    println("$year\n$hour\n$minute")
 
-
-    // Используем padStart чтобы выводить 09, а не 9.
-    // Иначе не даёт присваивать нули в начале целочисленных переменных
-
-    println("$year " +
-            "\n${hour.toString().padStart(2, '0')} " +
-            "\n${minute.toString().padStart(2, '0')} ")
-
-    hour = 10
-    minute = 55
-
+    hour = "10"
+    minute = "55"
     println("$hour:$minute")
 }
