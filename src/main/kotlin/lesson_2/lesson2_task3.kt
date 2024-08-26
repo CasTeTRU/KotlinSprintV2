e35f4b671c44ed72c663daa0a8234cc2206ff1fc
CasTeT <kongalkin@gmail.com>
2024-08-26 23:14:09 +0300

    KS-2-3 Train Convert minutes to hours and output time of arrival

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -4,9 +4,7 @@ fun main() {
     val departureHour: Short = 9
     val departureMinute: Short = 39
     val travelTime: Short = 457
-
     val arrivalHour: Int = (departureHour * 60 + departureMinute + travelTime) / 60
     val arrivalMinute: Int = (departureHour * 60 + departureMinute + travelTime) % 60
-
     println("$arrivalHour:$arrivalMinute")
 }
