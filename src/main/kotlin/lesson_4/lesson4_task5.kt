bb873f2115feedec5ac2328a4564fe87eb70f599
CasTeT <kongalkin@gmail.com>
2024-08-27 15:04:15 +0300

    KS-4-5-Checking-ship-readiness Проверяем готовность корабля к плаванию

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -6,10 +6,11 @@ fun main() {
     println("Введи число членов экипажа:")
     var crewNumber:Int = readln().toInt()
     println("Введи число ящиков провизии на борту:")
-    var provisionBoxes = readln()
+    var provisionBoxes = readln().toInt()
     println("Погода благоприятная? Отвечай: true/false")
-    var isWeatherIsGood = readln()
+    var isWeatherIsGood = readln().toBoolean()
     println("$isShipDamaged,$crewNumber,$provisionBoxes,$isWeatherIsGood")
 
-
+    val isShipReady = !isShipDamaged && (55 <= crewNumber <= 70) && provisionBoxes > 50 && $isWeatherIsGood
+    println()
 }
