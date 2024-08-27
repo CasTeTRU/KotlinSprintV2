5512dd870d1857d6f42146329434203b00faae47
CasTeT <kongalkin@gmail.com>
2024-08-27 16:21:54 +0300

    KS-2-1-Average-Score Убрал магические числа добавив новые переменные

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,6 +1,12 @@
 package org.example.lesson_2
 
 fun main() {
-    val averageScore: Double = (3.0 + 4.0 + 3.0 + 5.0) / 4
+    val firstPupilScore = 3.0
+    val secondPupilScore = 4.0
+    val thirdPupilScore = 3.0
+    val fourthPupilScore = 5.0
+    val numberOfPupils = 4
+    val averageScore: Double = (firstPupilScore + secondPupilScore
+            + thirdPupilScore + fourthPupilScore) / numberOfPupils
     println(averageScore)
 }
