ee863f09c82d446029a3857ed2d2f083b45f0d17
CasTeT <kongalkin@gmail.com>
2024-08-27 18:16:06 +0300

    KS-1-5-convert-seconds-to-hours-and-minutes Переделан вывод так что .format теперь применяется к строке вывода, а не к каждой переменной по отдельности

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,13 +1,12 @@
 package org.example.lesson_1
 
-import java.text.DecimalFormat
-
 fun main() {
     val secondsInSpace = 6480
     val secondsInHour = 3600
     val secondsInMinute = 60
-    val hoursInSpace = String.format("%02d", secondsInSpace / secondsInHour)
-    val minutesLeft = String.format("%02d", secondsInSpace % secondsInHour / secondsInMinute)
-    val secondsLeft = String.format("%02d", secondsInSpace % secondsInMinute)
-    println("$hoursInSpace:$minutesLeft:$secondsLeft")
+
+    val hoursInSpace = secondsInSpace / secondsInHour
+    val minutesLeft = secondsInSpace % secondsInHour / secondsInMinute
+    val secondsLeft = secondsInSpace % secondsInMinute
+    println("%02d:%02d:%02d".format(hoursInSpace, minutesLeft, secondsLeft))
 }
