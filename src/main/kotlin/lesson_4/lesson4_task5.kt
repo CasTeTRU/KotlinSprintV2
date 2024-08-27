ddd38c172a1ff821c0c56cdfb2721fd92692a57d
CasTeT <kongalkin@gmail.com>
2024-08-27 15:36:38 +0300

    KS-4-5-Checking-ship-readiness Проверяем готовность корабля к плаванию по условиям наличия повреждений, количества экипажа, провизии и состояния погоды

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,16 +1,20 @@
 package org.example.lesson_4
 
 fun main() {
-    println("Корабль имеет повреждения? Отвечай: true/false")
-    var isShipDamaged:Boolean = readln().toBoolean()
-    println("Введи число членов экипажа:")
-    var crewNumber:Int = readln().toInt()
-    println("Введи число ящиков провизии на борту:")
-    var provisionBoxes = readln().toInt()
-    println("Погода благоприятная? Отвечай: true/false")
-    var isWeatherIsGood = readln().toBoolean()
-    println("$isShipDamaged,$crewNumber,$provisionBoxes,$isWeatherIsGood")
+    val minCrewNumber = 55
+    val optimalCrewNumber = 70
+    val minProvisionBoxes = 50
 
-    val isShipReady = !isShipDamaged && (55 <= crewNumber <= 70) && provisionBoxes > 50 && $isWeatherIsGood
-    println()
+    println("Корабль имеет повреждения? Отвечай: true/false")
+    val isShipDamaged:Boolean = readln().toBoolean()
+    println("Введи число членов экипажа:")
+    val crewNumber:Int = readln().toInt()
+    println("Введи число ящиков провизии на борту:")
+    val provisionBoxes = readln().toInt()
+    println("Погода благоприятная? Отвечай: true/false")
+    val isWeatherIsGood = readln().toBoolean()
+
+    val isShipReady = (!isShipDamaged && crewNumber in minCrewNumber .. optimalCrewNumber && provisionBoxes > minProvisionBoxes)
+                       || (isShipDamaged && crewNumber == optimalCrewNumber && provisionBoxes >= minProvisionBoxes && isWeatherIsGood)
+    println("Корабль готов к плаванию : $isShipReady")
 }
