a8d2f16ba602a6f86f22e22f26fdc09f155c8f94
CasTeT <kongalkin@gmail.com>
2024-08-27 14:16:45 +0300

    KS-4-4-Execrsises for different parts Делаем вывод определенных групп мышц в зависимости от дня тренировки

---
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_4
+
+fun main() {
+    val exercisesArmABS = "старт"
+    val exercisesLegsSpine = "нестарт"
+    val dayOfExercises = 5
+
+
+/*    println("Упражнения для рук: $dayOfExercises=5 && $exercisesArmABS == "старт"\n
+    Упражнения для ног:      {$dayOfExercises=5 && $exercisesArmABS == "старт"}
+    Упражнения для спины:  {$dayOfExercises=5 && $exercisesArmABS == "старт"}
+    Упражнения для пресса: {$dayOfExercises=5 && $exercisesArmABS == "старт"}" */
+}
