ec0dac5b85cecb53ffa934675af93766c6bb02bc
CasTeT <kongalkin@gmail.com>
2024-08-27 16:15:38 +0300

    KS-3-1 Greeting Приветствуем пользователя в зависимости от времени суток

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -2,8 +2,8 @@ package org.example.lesson_3
 
 fun main() {
     val userName = "Василий"
-    var greetingText = "Добрый день, "
-    println(greetingText + userName + "!")
-    greetingText = "Добрый вечер, "
-    println(greetingText + userName + "!")
+    var greetingText = "Добрый день,"
+    println("$greetingText $userName!")
+    greetingText = "Добрый вечер,"
+    println("$greetingText $userName!")
 }
