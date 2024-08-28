f11e945b31471466be37694ed8d2205f482edf1a
CasTeT <kongalkin@gmail.com>
2024-08-28 17:31:23 +0300

    KS-4-1-Tables-reservations Вынесено значение общего числа столов в константу

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_4
+const val TOTAL_AMOUNT_OF_TABLES = 13
 
 fun main() {
-    val totalAmountOfTables = 13
-    val reservedTablesToday = 13
+      val reservedTablesToday = 13
     val reservedTablesTomorrow = 9
 
-    println("[Доступность столиков на сегодня: ${reservedTablesToday < totalAmountOfTables}]" +
-            "\n[Доступность столиков на завтра: ${reservedTablesTomorrow < totalAmountOfTables}]")
+    println("[Доступность столиков на сегодня: ${reservedTablesToday < TOTAL_AMOUNT_OF_TABLES}]" +
+            "\n[Доступность столиков на завтра: ${reservedTablesTomorrow < TOTAL_AMOUNT_OF_TABLES}]")
 }
