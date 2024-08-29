c67fd6f8c758ce0970c9bd43139081dda12b272a
CasTeT <kongalkin@gmail.com>
2024-08-29 21:39:50 +0300

    KS-5-2-Check if over 18 Получаем год рождения, вычитаем текущий год из года рождения, если больше 18 выводим сообщение, иначе ничего не выводим.

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_5
+const val AGE_OF_MAJORITY = 18
+const val CURRENT_YEAR = 2024
 
 fun main() {
-    val currentYear = 2024
     println("Введи год своего рождения:")
-    val yearOfBirth = readLine()!!.toInt()
-    val resultText = if (currentYear - yearOfBirth >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
-    else ""
+    val yearOfBirth = readln().toInt()
+
+    val resultText = if (CURRENT_YEAR - yearOfBirth >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
+    else return
+
     println(resultText)
 }
-
-const val AGE_OF_MAJORITY = 18
